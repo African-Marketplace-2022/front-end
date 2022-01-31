a48dae9dd36046e2aa53be719d08f3f166a73457
jessica <fuerte.jessica10@gmail.com>
2022-01-31 16:17:09 -0600

    added basic styling to homepage

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,26 @@
 import React from "react";
+import HomeImg from '../Images/vegetables-img.jpg';
+import "../css/Landing.css";
+
 
 const Landing = () => {
-  return <div>Landing Page</div>;
+
+
+
+
+
+  
+  return <div>
+    
+    <div className="home-img">
+      <img src={HomeImg} alt="vegetables" />
+      </div>
+      <div className="body-text">
+      <h2>What were about:</h2>
+      <h3>We empower women-led small and medium-sized enterprises to trade to legally, safely, and profitably across Africa. </h3>
+    </div>
+    <button id="landing-login" >Login To Start!</button>
+  </div>;
 };
 
 export default Landing;
